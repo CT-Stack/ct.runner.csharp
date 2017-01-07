8fa31468616a182a5b245f47729fa563637a634b
Grzegorz <dziedzic.m.grzegorz@gmail.com>
2017-01-07 15:32:54 +0100

    CT-16 Command for running dotnet watch
    Printing file paths and particular lines

---
+++ ct-runner-csharp/src/commands/WatchAndTestCommand.ts
@@ -0,0 +1,66 @@
+import {ICommand} from "./ICommand"
+import * as vscode from 'vscode';
+import * as childProc from 'child_process'
+
+export class WatchAndTestCommand implements ICommand {
+    public static get EXIT_TEXT(): string { return "dotnet exit code"; }
+    public static get STACK_TRACE_TEXT(): string { return "Stack Trace:"; }
+
+    buffer: string[]
+
+    constructor() {}
+
+    public execute(): void {
+        // rmdir /s /q &&
+        var child = childProc.spawn('dotnet watch test -parallel none -quiet', [],
+            {
+                cwd: 'C:\\Users\\Grzegorz\\Desktop\\dotnetcore\\tests',
+                shell: true
+            }
+        );
+        var self = this;
+        child.stdout.on('data', function(data) {
+            self.readCallback(data);
+        });
+
+        process.on('exit', function () {
+            child.kill();
+        });
+    }
+
+    private readCallback(data): void {
+        var splitted = data.toString().split('\n').filter(function(value, index, array) {
+            return value.localeCompare('');
+        });
+
+        for (var i = 0; i < splitted.length; i++)
+        {
+            this.buffer.push(splitted[i]);
+            if (splitted[i].search(WatchAndTestCommand.EXIT_TEXT) != -1)
+            {
+                var exit_code = parseInt(splitted[i][splitted[i].length - 2]);
+                if (exit_code != 0) {
+                    this.getFilesInfo(this.buffer);
+                }
+                break;
+            }
+        }
+    }
+
+    private getFilesInfo(buffer): void {
+        for (var i = 0; i < buffer.length; i++) {
+            var pos = buffer[i].search(WatchAndTestCommand.STACK_TRACE_TEXT);
+            if (pos != -1) {
+                var filePathLine = buffer[++i];
+                var fileNameEnd = filePathLine.indexOf('(', filePathLine.lastIndexOf('\\')); //for windows
+                var lineNumberStringStart = fileNameEnd + 1;
+                var lineNumberStringEnd = filePathLine.indexOf(',', lineNumberStringStart);
+                var filePath = filePathLine.substring(0, fileNameEnd);
+                var lineNumber = filePathLine.substring(lineNumberStringStart, lineNumberStringEnd);
+                var result = [filePath, lineNumber];
+                console.log (`${result[0]} : ${result[1]}`);
+            }
+        }
+        this.buffer.length = 0;
+    }
+}
--- ct-runner-csharp/src/extension.ts
+++ ct-runner-csharp/src/extension.ts
@@ -3,6 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import {WatchAndTestCommand} from "./commands/WatchAndTestCommand";
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -11,17 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ct-runner-csharp" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    var disposable = vscode.commands.registerCommand('watchAndTest', () => {
+        var watchAndTestCmmand = new WatchAndTestCommand();
+        watchAndTestCmmand.execute();
+	});
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
